d1aba8c0d124cc2b6dec5ad924e7b92768c512f6
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-06-15 08:44:04 -0300

    Criando backend clube

--- backend/clube/index.php
+++ backend/clube/index.php
@@ -2,3 +2,13 @@
     header('Content-type: application/json');
 
     $cxPdo = new PDO('mysql:host=localhost;port=3306;dbname=campeonato_futbol','root','');
+    $cmdSql = "SELECT * FROM clube";
+    $cxPrepare = $cxPdo->prepare($cmdSql);
+    $dados = [];
+    if($cxPrepare->execute()){
+        if($cxPrepare->rowCount() > 0){
+            $dados = $cxPrepare->fetchAll();
+        }
+    }
+
+    echo json_encode($dados);
