054f19638d6e452239d72d03e4f5b22a033153e3
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-06-15 09:20:07 -0300

    Criando consulta com parâmetor ao backend

--- backend/clube/index.php
+++ backend/clube/index.php
@@ -1,13 +1,29 @@
 <?php
     header('Content-type: application/json');
 
+    $dadosEnviados = file_get_contents('php://input');
+    $dadosEnviados = json_decode($dadosEnviados, true);
+
     $cxPdo = new PDO('mysql:host=localhost;port=3306;dbname=campeonato_futbol','root','');
+    
     $cmdSql = "SELECT * FROM clube";
+
+    if(isset($dadosEnviados['busca'])){
+        $busca = $dadosEnviados['busca'];
+        $cmdSql = "SELECT * FROM clube WHERE clube.nome LIKE '%$busca%'";
+    }
+       
     $cxPrepare = $cxPdo->prepare($cmdSql);
-    $dados = [];
+    $dados = [
+        'result'=>false,
+        'cmdExec'=>$dadosEnviados,
+        'dados' => []
+    ];
+
     if($cxPrepare->execute()){
         if($cxPrepare->rowCount() > 0){
-            $dados = $cxPrepare->fetchAll();
+            $dados['result'] = true;
+            $dados['dados'] = $cxPrepare->fetchAll();
         }
     }
 
