eb28e0c7318c16858a7224783e550d34de0dca8a
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-06-21 20:20:07 -0300

    Aula03

--- backend/clube/index.php
+++ backend/clube/index.php
@@ -1,6 +1,5 @@
 <?php
     header('Content-type: application/json');
-
     $dadosEnviados = file_get_contents('php://input');
     $dadosEnviados = json_decode($dadosEnviados, true);
 
@@ -12,18 +11,17 @@
         $busca = $dadosEnviados['busca'];
         $cmdSql = "SELECT * FROM clube WHERE clube.nome LIKE '%$busca%'";
     }
-       
+
     $cxPrepare = $cxPdo->prepare($cmdSql);
     $dados = [
         'result'=>false,
-        'cmdExec'=>$dadosEnviados,
-        'dados' => []
+        'valores' => []
     ];
 
     if($cxPrepare->execute()){
         if($cxPrepare->rowCount() > 0){
             $dados['result'] = true;
-            $dados['dados'] = $cxPrepare->fetchAll();
+            $dados['valores'] = $cxPrepare->fetchAll();
         }
     }
 
